d02463c392ef094dfa0f434159bdbca00101e793
FallAngel1337 <fallangel@protonmail.com>
2022-08-21 11:36:09 -0300

    Add create tarball function

--- crates/ktar_lib/src/tar/mod.rs
+++ crates/ktar_lib/src/tar/mod.rs
@@ -1,15 +1,34 @@
-use std::io::Result as IoResult;
+use std::io::{prelude::*, Result as IoResult};
+use std::fs::File;
 use std::path::PathBuf;
 use crate::{Archive, ArchiveFile};
 
 impl Archive {
-    pub fn create(files: &[PathBuf]) -> IoResult<Vec<u8>> {
-        let mut _files = vec![];
+    fn tarball(files: &[PathBuf]) -> IoResult<Vec<u8>> {
+        let mut tarball = vec![];
         for file in files {
-            _files.push(ArchiveFile::new(file)?);
-        }
-        let files = _files;
+            let archive = ArchiveFile::new(file)?;
+            let (
+                file_name,
+                file_name_len,
+                file_size,
+                file_contemts
+            ) = (
+                archive.file_name.as_bytes(),
+                &archive.file_name.len().to_le_bytes(),
+                &archive.file_size.to_le_bytes(),
+                &archive.contents);
 
-        Ok(vec![])
+            tarball.extend_from_slice(&[file_name_len, file_name, file_size, file_contemts].concat())
+        }
+
+        Ok(tarball)
+    }
+
+    pub fn create(files: &[PathBuf], output: PathBuf) -> IoResult<()> {
+        let tarball = Self::tarball(files)?;
+        let mut file = File::create(output)?;
+        file.write_all(&tarball)?;
+        Ok(())
     }
 }
