e21dd3e3470c95ed0d28c80e23944ccfe69fecc0
FallAngel1337 <fallangel@protonmail.com>
2022-08-21 08:49:38 -0300

    Add archive structure

--- crates/ktar_lib/src/lib.rs
+++ crates/ktar_lib/src/lib.rs
@@ -1,2 +1,40 @@
+use std::io::{Result as IoResult, Read};
+use std::os::linux::fs::MetadataExt;
+use std::path::{PathBuf, Path};
+use std::fs::{Metadata, File};
+
 mod tar;
 mod untar;
+
+pub struct ArchiveFile {
+    file_name: PathBuf,
+    metadata: Metadata,
+    content: Vec<u8>,
+}
+
+pub struct Archive {
+    pub files: Vec<ArchiveFile>,
+}
+
+impl ArchiveFile {
+    fn new<T: AsRef<Path>>(file_name: &T) -> IoResult<Self> {
+        let mut file = File::open(file_name)?;
+        let metadata = file.metadata()?;
+        let file_size = metadata.st_size() as usize;
+        let mut content = vec![0u8; file_size];
+        
+        if file.read_to_end(&mut content)? != file_size {
+            eprintln!(r"\`st_size\` and \`file_size\` are not equal");
+            std::process::exit(1);
+        }
+        
+
+        Ok(
+            Self {
+                file_name: file_name.as_ref().to_path_buf(),
+                metadata,
+                content,
+            }
+        )
+    }
+}
