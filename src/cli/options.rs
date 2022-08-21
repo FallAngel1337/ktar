af78c4ab2f6dec2d7c46d26fcfc4d6550f0c5ba3
FallAngel1337 <fallangel@protonmail.com>
2022-08-21 10:17:41 -0300

    Fixed cli interface

--- src/cli/options.rs
+++ src/cli/options.rs
@@ -4,11 +4,11 @@ use clap::{Parser, ValueHint};
 #[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
 pub struct Cli {
-    #[clap(short = 'x', help = "Extract *.tar file(s)")]
-    pub extract: bool,
+    #[clap(short = 'x', name = "dest", help = "Extract *.tar file(s) to <dest>")]
+    pub extract: Option<PathBuf>,
     
-    #[clap(short = 't', help = "Create a tar file")]
-    pub create: bool,
+    #[clap(short = 't', name = "output", help = "Create a tar file called <output>")]
+    pub create: Option<PathBuf>,
 
     #[clap(name = "FILE", parse(from_os_str), value_hint = ValueHint::AnyPath, required = true)]
     pub files: Vec<PathBuf>,
